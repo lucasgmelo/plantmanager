56dbfa7184c1df3e8c2a2e33e701d85f05fd76b5
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-22 00:48:58 -0300

    :sauropod: create confirmation page

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import AppLoading from 'expo-app-loading'
 import { Welcome } from './src/pages/Welcome';
 import {useFonts, Inter_400Regular, Inter_600SemiBold } from '@expo-google-fonts/inter'
 import { UserIdentification } from './src/pages/UserIdentification';
+import { Confirmation } from './src/pages/Confirmation';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -15,6 +16,6 @@ export default function App() {
   )
 
   return (
-    <UserIdentification />
+    <Confirmation />
   );
 }
