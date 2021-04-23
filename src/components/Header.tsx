fdfb95c30a2959ccdb407cfdca9a3a72f8a1d35e
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-23 18:37:11 -0300

    :sauropod: create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import { 
-    View,
-    Text,
-    StyleSheet,
-    Image
-} from 'react-native';
-import colors from '../../styles/colors';
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
-export function Header(){
-    return (
-        <View style={styles.container}>
-        </View>
-    )
+import userImg from "../assets/luc.jpeg";
+import colors from "../../styles/colors";
+import fonts from "../../styles/fonts";
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.username}>Lucas</Text>
+      </View>
+      <Image style={styles.image} source={userImg} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingVertical: 20,
-        backgroundColor: colors.red,
-    }
-})
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: getStatusBarHeight(),
+    padding: 20,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  username: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 40,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40
+  }
+});
