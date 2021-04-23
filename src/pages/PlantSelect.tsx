55997ee78ff18bd3168f0f67ee489e90b95089b4
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-23 19:45:17 -0300

    :sauropod: fetch data from api

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 import colors from "../../styles/colors";
 import fonts from "../../styles/fonts";
 import { EnviromentButton } from "../components/EnviromentButton";
 import { Header } from "../components/Header";
+import api from "../services/api";
+
+interface EnviromentProps {
+  key: string;
+  title: string;
+}
 
 export function PlantSelect() {
+  const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
+
+  useEffect(() => {
+    async function fetchEnviroment() {
+      try {
+        const { data } = await api.get("/plants_enviroments");
+        console.log(data)
+        setEnviroments([
+          {
+            key: "all",
+            title: "Todos",
+          },
+          ...data,
+        ]);
+      } catch {
+        setEnviroments([
+          {
+            key: "all",
+            title: "Todos",
+          },
+        ]);
+      }
+    }
+
+    fetchEnviroment();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Header />
@@ -15,8 +48,8 @@ export function PlantSelect() {
       </View>
       <View>
         <FlatList
-          data={[1, 2, 3, 4, 5, 6, 7]}
-          renderItem={({ item }) => <EnviromentButton title="Cozinha" active />}
+          data={enviroments}
+          renderItem={({ item }) => <EnviromentButton title={item.title} />}
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.enviromentList}
@@ -48,10 +81,10 @@ const styles = StyleSheet.create({
     color: colors.heading,
   },
   enviromentList: {
-      height: 40,
-      justifyContent: 'center',
-      paddingBottom: 5,
-      marginLeft: 32,
-      marginVertical: 32,
-  }
+    height: 40,
+    justifyContent: "center",
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
+  },
 });
