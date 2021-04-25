a704a7c86c3e1b95c9fa92f8e68a9c690af52c6a
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-24 23:15:51 -0300

    :sauropod: get name on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import userImg from "../assets/luc.jpeg";
 import colors from "../../styles/colors";
 import fonts from "../../styles/fonts";
 
 export function Header() {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    async function getName(){
+      const user = await AsyncStorage.getItem('@plantmanager:user')
+      setUsername(user || '');
+    }
+
+    getName();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.greeting}>Olá,</Text>
-        <Text style={styles.username}>Lucas</Text>
+        <Text style={styles.username}>{username}</Text>
       </View>
       <Image style={styles.image} source={userImg} />
     </View>
