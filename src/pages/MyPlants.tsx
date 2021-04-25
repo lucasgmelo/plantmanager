5fef8d58b315d5061bd4cbcbad6f4b84eda6346a
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-25 01:21:19 -0300

    :sauropod: load plants and format time distance

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -1,16 +1,59 @@
 // import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import colors from "../../styles/colors";
 import fonts from "../../styles/fonts";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 
+import waterDrop from "../assets/waterdrop.png";
+import { FlatList } from "react-native-gesture-handler";
+import { loadPlant, PlantProps } from "../libs/storage";
+import { formatDistance } from "date-fns";
+import { pt } from "date-fns/locale";
+
+
 export function MyPlants() {
+    const [plants, setPlants] = useState<PlantProps[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [nextWatered, setnextWatered] = useState('');
+
+    useEffect(() => {
+        async function loadStorageData(){
+            const plantsStoraged = await loadPlant();
+
+            const nextTime = formatDistance(
+                new Date(plantsStoraged[0].dateTimeNotification).getTime(),
+                new Date().getTime(),
+                { locale: pt}
+            );
+
+            setnextWatered(`
+            Não esqueça de regar a ${plantsStoraged[0].name} às ${nextTime} horas`)
+        }
+
+        loadStorageData();
+    }, []);
 
   return (
     <View style={styles.container}>
-        <Header />
+      <Header />
+
+      <View style={styles.spotlight}>
+        <Image styles={styles.spotlightImage} source={waterDrop} />
+        <Text style={styles.spotlightText}>qualquer coisa</Text>
+      </View>
+
+      <View style={styles.plants}>
+        <Text style={styles.plantsTitle}>
+            Próximas a serem regadas
+        </Text>
+
+        <FlatList 
+        data={}
+        />
+
+      </View>
     </View>
   );
 }
