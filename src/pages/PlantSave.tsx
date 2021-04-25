529b70f1cd7491c168dddd7e0c28fb78c5d4a80a
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-24 23:53:13 -0300

    :sauropod: get data throw pages

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -9,7 +9,10 @@ import {
   Platform,
   TouchableOpacity,
 } from "react-native";
-import { getBottomSpace, getStatusBarHeight } from "react-native-iphone-x-helper";
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from "react-native-iphone-x-helper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { SvgFromUri } from "react-native-svg";
 
@@ -19,7 +22,27 @@ import colors from "../../styles/colors";
 import { Button } from "../components/Button";
 import fonts from "../../styles/fonts";
 
+import { useRoute } from "@react-navigation/core";
+
+interface Params {
+  plant: {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+      times: number;
+      repeat_every: string;
+    };
+  };
+}
+
 export function PlantSave() {
+  const route = useRoute();
+  const { plant } = route.params as Params;
+  
   return (
     <View style={styles.container}>
       <View style={styles.plantInfo}>
@@ -34,8 +57,7 @@ export function PlantSave() {
         <View style={styles.tipContainer}>
           <Image source={waterDrop} style={styles.tipImage} />
           <Text style={styles.tipText}>
-            Lorem ipsum dolor sit amet.
-            quam? wtf men hahahahah
+            Lorem ipsum dolor sit amet. quam? wtf men hahahahah
           </Text>
         </View>
         <Text style={styles.alertLabel}>
@@ -55,11 +77,11 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
   },
   plantInfo: {
-      flex: 1,
-      paddingHorizontal: 30,
-      paddingVertical: 50,
-      alignItems: "center",
-      backgroundColor: colors.shape,
+    flex: 1,
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+    alignItems: "center",
+    backgroundColor: colors.shape,
   },
   controller: {
     backgroundColor: colors.white,
@@ -68,46 +90,45 @@ const styles = StyleSheet.create({
     paddingBottom: getBottomSpace() || 20,
   },
   plantName: {
-      fontFamily: fonts.heading,
-      fontSize: 24,
-      color: colors.heading,
-      marginTop: 15
+    fontFamily: fonts.heading,
+    fontSize: 24,
+    color: colors.heading,
+    marginTop: 15,
   },
   plantAbout: {
-      textAlign: "center",
-      fontFamily: fonts.text,
-      color: colors.heading,
-      fontSize: 17,
-      marginTop: 10,
+    textAlign: "center",
+    fontFamily: fonts.text,
+    color: colors.heading,
+    fontSize: 17,
+    marginTop: 10,
   },
   tipContainer: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      backgroundColor: colors.blue_light,
-      padding: 20,
-      borderRadius: 20,
-      position: 'relative',
-      bottom: 70,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: colors.blue_light,
+    padding: 20,
+    borderRadius: 20,
+    position: "relative",
+    bottom: 70,
   },
   tipImage: {
-      width: 56,
-      height: 56,
+    width: 56,
+    height: 56,
   },
   tipText: {
-      flex: 1,
-      marginLeft: 20,
-      fontFamily: fonts.text,
-      color: colors.blue,
-      fontSize: 17,
-      textAlign: 'justify',
+    flex: 1,
+    marginLeft: 20,
+    fontFamily: fonts.text,
+    color: colors.blue,
+    fontSize: 17,
+    textAlign: "justify",
   },
   alertLabel: {
-    textAlign: 'center',
+    textAlign: "center",
     fontFamily: fonts.complement,
     color: colors.heading,
     fontSize: 12,
-    marginBottom: 5
-
-  }
+    marginBottom: 5,
+  },
 });
